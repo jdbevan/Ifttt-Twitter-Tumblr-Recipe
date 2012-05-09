d94504e5cfbf609c422d36d18da1182b2b97859d
Jon Bevan <jon@jonbevan.me.uk>
2012-05-10 00:50:42 +0100

    Sample oauth config file

---
+++ oauth_config.sample.php
@@ -0,0 +1,9 @@
+<?php
+define("BASE_HOSTNAME", "");	// Domain name for your blog
+define("API_KEY", "");
+define("API_SECRET", "");
+define("TOKEN", "");			// These can be left blank
+define("TOKEN_SECRET", "");		// I think...
+
+date_default_timezone_set("Europe/London");
+?>
